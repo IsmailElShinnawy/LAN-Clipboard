f8aa0de74b9e20afb10490aaf99b138837ac9b86
IsmailElShinnawy <ismailelshennawy@gmail.com>
2021-02-11 11:16:30 +0200

    added item_delete

--- controllers/itemController.js
+++ controllers/itemController.js
@@ -3,13 +3,16 @@ const db = require('../model/database');
 const items_index = (req, res) => {
     db.all(`
     SELECT
+        I.item_id,
         I.text,
         datetime(I.timestamp, 'localtime') AS timestamp,
         U.login_name
     FROM
         item I
     INNER JOIN user U
-        ON I.user_id = U.user_id;
+        ON I.user_id = U.user_id
+    ORDER BY
+        timestamp DESC;
     `, [], (err, rows) => {
         if (err) {
             console.log(err);
@@ -33,13 +36,16 @@ const item_paste_post = (req, res) => {
             })
             .all(`
             SELECT
+                I.item_id,
                 I.text,
                 datetime(I.timestamp,'localtime') AS timestamp,
                 U.login_name
             FROM
                 item I
             INNER JOIN user U
-                ON I.user_id = U.user_id;
+                ON I.user_id = U.user_id
+            ORDER BY
+                timestamp DESC;
             `, [], (err, rows) => {
                 if (err) {
                     console.log(err);
@@ -48,11 +54,45 @@ const item_paste_post = (req, res) => {
                 res.render('index', { content: rows });
             });
     });
+}
 
+const item_delete = (req, res) => {
+    const delete_id = req.params.id;
+    db.serialize(() => {
+        db
+            .run(`
+            DELETE FROM item
+            WHERE item_id = ?;
+            `, [delete_id], (err) => {
+                if (err) {
+                    console.log(err);
+                }
+            })
+            .all(`
+            SELECT
+                I.item_id,
+                I.text,
+                datetime(I.timestamp,'localtime') AS timestamp,
+                U.login_name
+            FROM
+                item I
+            INNER JOIN user U
+                ON I.user_id = U.user_id
+            ORDER BY
+                timestamp DESC;
+            `, [], (err, rows) => {
+                if (err) {
+                    console.log(err);
+                    return;
+                }
+                res.json({ redirect: '/items' });
+            });
+    });
 }
 
 
 module.exports = {
     items_index,
     item_paste_post,
+    item_delete,
 }
